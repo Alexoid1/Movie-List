cdd515bb151b251c2268d158c130f0e1ea3a3c09
Alexoid1 <alex_zam@hotmail.es>
2020-03-25 20:26:43 -0500

    Updates

---
+++ precache-manifest.f8309a857a59f56456823f41dbcd4cc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f398d97550618fae5c462e2682e9594c",
+    "url": "/Movie-List/index.html"
+  },
+  {
+    "revision": "a52f7fcacba27f4968c1",
+    "url": "/Movie-List/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2404c772aa0f11ebd152",
+    "url": "/Movie-List/static/js/2.00db8640.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-List/static/js/2.00db8640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52f7fcacba27f4968c1",
+    "url": "/Movie-List/static/js/main.f6e70959.chunk.js"
+  },
+  {
+    "revision": "a66737cd3116c741280e",
+    "url": "/Movie-List/static/js/runtime-main.30477595.js"
+  }
+]);
